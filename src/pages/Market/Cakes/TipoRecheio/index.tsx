6e3352dc96a845273213be78281146554be0a2ef
Alisson Deivison Silva Pereira <uepbalisson@gmail.com>
2023-11-29 17:38:38 -0300

    Alterações no código para receber um objeto mais simplificado

--- src/pages/Market/Cakes/TipoRecheio/index.tsx
+++ src/pages/Market/Cakes/TipoRecheio/index.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import { Card } from '@rneui/themed';
 
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { collection, doc, getDocs, getFirestore, setDoc } from 'firebase/firestore';
@@ -20,16 +19,17 @@ export default function TipoRecheio({ onValueChange }) {
 
     const auth = getAuth();
 
-    const [recheiosData, setRecheiosData] = useState([])
-    const [recheioEscolhido, setRecheioEscolhido] = useState(null)
-    const [favorito, setFavorito] = useState({})
+    const [recheiosData, setRecheiosData] = useState([]);
+    const [recheioEscolhido, setRecheioEscolhido] = useState(null);
+    const [favorito, setFavorito] = useState([]);
+    const [favoritosDoUsuario, setFavoritosDoUsuario] = useState([]);
 
     let uid = null;
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
             uid = user.uid;
-            console.log(uid);
+            //console.log(uid);
         }
     });
 
@@ -42,9 +42,15 @@ export default function TipoRecheio({ onValueChange }) {
 
                 const recheiosCollection = collection(db, 'recheios');
                 const recheiosSnapshot = await getDocs(recheiosCollection);
-
                 const recheios = recheiosSnapshot.docs.map(doc => doc.data());
                 setRecheiosData(recheios);
+
+                const favoritosCollection = collection(db, 'informacoesDoUsuario');
+                const favoritosSnapshot = await getDocs(favoritosCollection);
+
+                const favoritosArray = favoritosSnapshot.docs.map(doc => doc.data().favorito);
+
+                setFavoritosDoUsuario(favoritosArray);
             } catch (error) {
                 console.error('Erro ao conectar ao servidor: ', error)
             }
@@ -61,23 +67,23 @@ export default function TipoRecheio({ onValueChange }) {
         });
     };
 
-
+    //Adiciona o favorito ao banco 
     const adicionarFavorito = async (favorito) => {
-
-        console.log('Favorito 1', favorito);
         const db = getFirestore(app);
         await setDoc(doc(db, 'informacoesDoUsuario', uid), { favorito });
-        console.log("Documento adicionado com ID: ", uid);
+        //console.log("Documento adicionado com ID: ", uid);
 
     };
 
+    // Apenas para depuração
+    // useEffect(() => { 
+    //     onValueChange(recheioEscolhido);
+    //     console.log(recheioEscolhido);
+    // }, [recheioEscolhido]) 
 
-    useEffect(() => {
-        onValueChange(recheioEscolhido);
-        console.log(recheioEscolhido);
-    }, [recheioEscolhido])
-
-
+    useEffect(()=>{
+        console.log(favoritosDoUsuario[1]);
+    },[favoritosDoUsuario])
 
     return (
         <View style={styles.container}>
@@ -88,7 +94,13 @@ export default function TipoRecheio({ onValueChange }) {
                         <Text style={styles.cardText}>{recheio.description}</Text>
                         <View style={styles.favorite}>
                             <TouchableOpacity onPress={() => toggleFavorito(i)}>
-                                {favorito[i] ? <AntDesign name="heart" size={24} color="red" /> : <AntDesign name="hearto" size={24} color="black" />}
+
+                                {favorito[i] ? (
+                                    <AntDesign name="heart" size={24} color="red" />
+                                ) : (
+                                    <AntDesign name="hearto" size={24} color="black" />
+                                )}
+
                             </TouchableOpacity>
                         </View>
                     </View>
